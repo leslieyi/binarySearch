81731a1aaeff9b93c5e521e46175a02d45486a6e
leslieyi <leslieyi411@gmail.com>
2021-11-18 13:50:58 -0500

    Afternoon

--- guessNumberHiLo.js
+++ guessNumberHiLo.js
@@ -12,22 +12,49 @@
 // Return the number that I picked.
 
 // Example 1:
-
-// Input: n = 10, pick = 6
+// Input: n = 10, pick = 6 //pick is like the TARGET number
 // Output: 6
-// Example 2:
 
+// Example 2:
 // Input: n = 1, pick = 1
 // Output: 1
-// Example 3:
 
+
+// Example 3:
 // Input: n = 2, pick = 1
 // Output: 1
-// Example 4:
 
+
+// Example 4:
 // Input: n = 2, pick = 2
 // Output: 2
 
-function guess(n) {
-    
-}
+
+//   Forward declaration of guess API.
+//  @param {number} num   your guess
+//   @return 	            -1 if num is lower than the guess number
+//  			             1 if num is higher than the guess number
+//                         otherwise return 0
+//   var guess = function(num) {}
+
+
+
+// @param {number} n
+//   @return {number}
+
+
+var guessNumber = function (n) {
+  let start = 0;
+  let end = n;
+
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    if (guess(middle) === 0) return middle;
+    else if (guess(middle) === -1) {
+      //-1: The number I picked is lower than your guess (i.e. target < num)
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+};
